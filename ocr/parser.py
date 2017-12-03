dcc38db368951aac470dbb78ac75d6c092c349bf
Josh Shank <josh.shank@fomalhaut.local>
2017-12-03 05:19:34 -0500

    Completed README.md and code documentation

--- ocr/parser.py
+++ ocr/parser.py
@@ -2,16 +2,57 @@ from re import compile
 
 
 class BusinessCardParser(object):
+    """A document parser that builds _ContactInfo objects
+
+    Attributes
+    ----------
+    _email_expr : pattern object
+        A regex pattern to validate email addresses
+    """
 
     def __init__(self):
         self._email_expr = compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 
     def get_contact_info(self, document):
+        """Initial processing of business card document
+
+        Note
+        ----
+        Processing is redirected to the _filter_fields() method
+
+        Parameters
+        ----------
+        document : str
+            A multiline document compressed into a string object
+
+        Returns
+        -------
+        :obj:`_ContactInfo`
+            A populated _ContactInfo object with the extracted data
+
+        """
         fields = self._filter_fields(document.split('\n'))
 
         return _ContactInfo(**fields)
 
     def _filter_fields(self, document):
+        """Extracts all _ContactInfo fields from a documents
+
+        Note
+        ----
+        The field_map is populated with the attributes set in _ContactInfo.__slots__
+
+        Parameters
+        ----------
+        document : :obj:`list` of :obj:`str`
+            A list containing each line of the document passed in
+
+        Returns
+        -------
+        :obj:`dict`
+            A dictionary to be passed as the **kwargs to a _ContactInfo initializer
+
+        """
         field_map = {k[2:]: None for k in _ContactInfo.__slots__}
 
         for line in document:
@@ -40,6 +81,23 @@ class BusinessCardParser(object):
 
 
 class _ContactInfo(object):
+    """Data class to store business card info
+
+    Note
+    ----
+    No attribute manipulation should take place in this class.
+    All attributes should be stored in __slots__ for use in the BusinessCardParser.
+    All getters call str() on attributes to handle non-string objects
+
+    Parameters
+    ----------
+    __name : str
+        Name field of a business card document
+    __phone : str
+        Phone number field of a business card document
+    __email : str
+        Email address field of a business card document
+    """
 
     __slots__ = ("__name", "__phone", "__email")
 
@@ -49,15 +107,19 @@ class _ContactInfo(object):
         self.__email = kwargs["email"]
 
     def __str__(self):
+        """Special format string output with every field populated"""
         return f"Name: {self.get_name()}\n" \
                f"Phone: {self.get_phone_number()}\n" \
                f"Email: {self.get_email_address()}"
 
     def get_name(self):
+        """Return str of __name attribute"""
         return str(self.__name)
 
     def get_phone_number(self):
+        """Return str of __phone attribute"""
         return str(self.__phone)
 
     def get_email_address(self):
+        """Return str of __email attribute"""
         return str(self.__email)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,24 +8,42 @@ from ocr import BusinessCardParser
 logger = logging.getLogger(__name__)
 
 class TestParser(unittest.TestCase):
+    """A tester for the parser module with BusinessCardParser"""
 
     def setUp(self):
+        """Setup the TestCase prior to execution"""
         self.parser = BusinessCardParser()
         logger.debug(f"Setting up {self.__class__}")
 
     def tearDown(self):
+        """Formally delete internal object attributes"""
         logger.debug(f"Tearing down {self.__class__}")
         del self.parser
 
     def test_asymmetrik(self):
+        """Run the ./samples/asymmetrik.yaml test set"""
         logger.info(f"Running Asymmetrik samples test!")
         self._check_examples(f"{dirname(realpath(__file__))}/samples/asymmetrik.yaml")
 
     def test_custom(self):
+        """Run the ./samples/custom.yaml test set"""
         logger.info(f"Running custom samples test!")
         self._check_examples(f"{dirname(realpath(__file__))}/samples/custom.yaml")
 
     def _check_examples(self, file):
+        """Load a set of tests and run them through the BusinessCardParser
+
+        Note
+        ----
+        Load a YAML test file and test the processed documents against
+        there expected output.
+
+        Parameters
+        ----------
+        file : str
+            The test file path to load
+
+        """
         with open(file, 'r') as f:
             examples = yaml.safe_load(f).values()
 
